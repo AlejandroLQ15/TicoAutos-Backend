73cb3324033af27dab1028d8c39f452b329f1468
danicalde2003 <danicalde2003@gmail.com>
2026-03-17 00:39:07 -0600

    Archivos donde manejaremos los atributos de cada objeto

--- models/users.js
+++ models/users.js
@@ -2,8 +2,9 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
     username: { required: true, type: String, unique: true },
-    password: { required: true, type: String }, // Por ahora texto plano, luego JWT
-    nombre: { required: true, type: String }
+    password: { required: true, type: String },
+    nombre: { required: true, type: String },
+    foto_perfil: { type: String, default: null }
 });
 
 module.exports = mongoose.model('User', userSchema);    
